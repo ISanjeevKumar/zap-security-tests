8d65f01940c0e936d72afcdfa48ff2607dab5fc2
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2021-11-18 23:37:47 +0530

    Added Spider Scan Test

--- src/test/java/com/sanjeev/controller/ZapClientController.java
+++ src/test/java/com/sanjeev/controller/ZapClientController.java
@@ -1,5 +1,63 @@
 package com.sanjeev.controller;
 
+import org.zaproxy.clientapi.core.ApiResponse;
+import org.zaproxy.clientapi.core.ApiResponseElement;
+import org.zaproxy.clientapi.core.ClientApi;
+import org.zaproxy.clientapi.core.ClientApiException;
+
 public class ZapClientController {
-    
+
+    private ClientApi clientApi;
+    private String targetUrl;
+
+    public ZapClientController(String localhost, int port, String targetUrl) {
+        this.targetUrl = targetUrl;
+        clientApi = new ClientApi(localhost, port);
+    }
+
+    public String startSpiderScan() {
+        try {
+            ApiResponse response = clientApi.spider.scan(targetUrl, null, null, null, null);
+            String scanid = ((ApiResponseElement) response).getValue();
+            return scanid;
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public void waitTillSpiderScanIsCompleted(String scanId)
+            throws InterruptedException, NumberFormatException, ClientApiException {
+        int progress;
+        while (true) {
+            Thread.sleep(1000);
+            progress = Integer.parseInt(((ApiResponseElement) clientApi.spider.status(scanId)).getValue());
+            System.out.println("Spider progress : " + progress + "%");
+            if (progress >= 100) {
+                break;
+            }
+        }
+        System.out.println("Spider completed");
+    }
+
+    public void generateHtmlReport() throws ClientApiException {
+        String title = "JuiceShop App";
+        String template = "traditional-html-plus";
+        String theme = "light";
+        String description = " JuiceShop Tests";
+        String contexts = "";
+        String sites = "";
+        String sections = "";
+        String includedconfidences = "";
+        String includedrisks = "";
+        String reportfilename = "";
+        String reportfilenamepattern = "";
+        String reportdir = "./zap-security-tests/reports";
+        String display = "";
+
+        clientApi.reports.generate(title, template, theme, description, contexts, sites, sections, includedconfidences,
+                includedrisks, reportfilename, reportfilenamepattern, reportdir, display);
+        System.out.println("Report generated");
+        // File.(reportFileName + ".html", clientApi.reports.generate());
+    }
 }
--- src/test/java/com/sanjeev/tests/ZapTests.java
+++ src/test/java/com/sanjeev/tests/ZapTests.java
@@ -1,21 +1,37 @@
 package com.sanjeev.tests;
 
+import com.sanjeev.controller.ZapClientController;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.zaproxy.clientapi.core.ClientApiException;
 
 public class ZapTests {
-    
-	@Test(description="Scan JuiceShop application - baseline scan")
-	public void loginTestFacebook() {
-		
-		try 
-		{
-			{
-                    System.out.println("Hello world");
-			}
-		}
-		catch(Exception ex) 
-		{
-			ex.printStackTrace();
-		}
-	}
+
+    private ZapClientController zap;
+
+    @BeforeTest
+    public void instantiateZapClient() {
+        zap = new ZapClientController("localhost", 8081, "http://localhost:3000");
+    }
+
+    @AfterTest
+    public void disposeClientApi() throws ClientApiException {
+        zap.generateHtmlReport();
+    }
+
+    @Test(description = "Start Spider Scan against JuiceShop application")
+    public void spiderScanTest() {
+
+        try {
+            {
+                String scanId = zap.startSpiderScan();
+                System.out.println("Spider ScanId:" + scanId);
+                zap.waitTillSpiderScanIsCompleted(scanId);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
